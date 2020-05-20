ff660f6f513ee3d942e0891c07f9cdf5806548d4
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-05-20 16:48:59 -0400

    Add login component and Ternay operator for app.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,14 @@
 import React from "react";
+import Login from "./Login";
 
-var isLoggegIn = true;
-
-function changeConditionally(){
-
-if (isLoggegIn){
-  return <h1>Hello</h1>
-}else {
-  return <form className="form">
-    <input type="text" placeholder="Username" />
-    <input type="password" placeholder="Password" />
-    <button type="submit">Login</button>
-  </form>
-}
-
-}
+var isLoggedIn = false;
 
 
 function App() {
   return (
     <div className="container">
-       {changeConditionally()}
+      {/*Ternary Operator*/}
+      {isLoggedIn ? <h1>Hello</h1> : <Login />}
     </div>
   );
 }
