5ce2fe394bfa943887691585128f85d0aeda18e9
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-05-20 14:47:56 -0400

    Add template for sign in page

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+var isLoggegIn = true;
+
+function changeConditionally(){
+
+if (isLoggegIn){
+  return <h1>Hello</h1>
+}else {
+  return <form className="form">
+    <input type="text" placeholder="Username" />
+    <input type="password" placeholder="Password" />
+    <button type="submit">Login</button>
+  </form>
+}
+
+}
+
+
+function App() {
+  return (
+    <div className="container">
+       changeConditionally()
+    </div>
+  );
+}
+
+export default App;
